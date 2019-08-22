022aa01f8fde2f8499459f718784bc4d99cefcbe
lz <ludizh@gmail.com>
2019-08-21 20:33:35 -0700

    Update 79_word_search_v1.py

--- 79_word_search_v1.py
+++ 79_word_search_v1.py
@@ -1,6 +1,7 @@
 """
 lz@2019/08/17
 Time Limit Exceeded
+"""
 
 import copy
 class Solution:
@@ -66,39 +67,5 @@ class Solution:
             else:
                 answer[key].append((i, j + 1))
         return(answer)
-"""
 
-"""
-Improved version:
-"""
-class Solution:
-    def exist(self, board, word):
-        if len(word) < 1:
-            return(False)
-        m, n = len(board), len(board[0])
-        visited = [[0]*n]*m
-        
-        for i in range(m):
-            for j in range(n):
-                if self.existSubstr(board, word, i, j, m, n, visited):
-                    return(True)
-        return(False)
-        
-
-    def existSubstr(self, board, substr, i, j, m, n, visited):
-        if substr == '':
-            return(True)
-        if i < 0 or j < 0 or i >= m or j >= n or visited[i][j] or board[i][j] != substr[0]:
-            return(False)
-        
-        visited[i][j] = 1
-        
-        if self.existSubstr(board, substr[1:], i - 1, j, m, n, visited) \
-        or self.existSubstr(board, substr[1:], i, j - 1, m, n, visited) \
-        or self.existSubstr(board, substr[1:], i + 1, j, m, n, visited) \
-        or self.existSubstr(board, substr[1:], i, j + 1, m, n, visited):
-            return(True)
-        
-        visited[i][j] = 0
-        return(False)
 
