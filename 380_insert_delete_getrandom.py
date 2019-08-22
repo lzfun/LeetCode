14c9f35e84028ba02cab578d4e5a86e903cfbf1a
lz <ludizh@gmail.com>
2019-08-21 22:16:21 -0700

    Update 380_insert_delete_getrandom.py

--- 380_insert_delete_getrandom.py
+++ 380_insert_delete_getrandom.py
@@ -5,6 +5,7 @@ Created on Wed Aug 21 21:50:53 2019
 
 @author: lz
 """
+import random
 class RandomizedSet:
 
     def __init__(self):
@@ -19,19 +20,32 @@ class RandomizedSet:
         """
         Inserts a value to the set. Returns true if the set did not already contain the specified element.
         """
-        if val in self.values:
-            return(False)
-        else:
-            loc = len(self.values)
-            self.values.append(val)
-            return(True)
+        loc = self.dict.get(val, None)
         
-
+        if loc is None:
+            loc = len(self.values)
+            
+            self.dict[val] = loc
+            self.values.append(val)
+            
+            return(True)
+        else:
+            return(False)
+        
     def remove(self, val: int) -> bool:
         """
         Removes a value from the set. Returns true if the set contained the specified element.
         """
-        if val in self.values:
+        loc = self.dict.get(val, None)
+        
+        if loc is not None:
+            temp = self.values[-1]
+            
+            self.dict[temp] = loc
+            del self.dict[val]
+            
+            self.values[loc] = self.values[-1]
+            self.values.pop()
             return(True)
         else:
             return(False)
@@ -41,11 +55,17 @@ class RandomizedSet:
         """
         Get a random element from the set.
         """
-        
+        return(self.values[random.randrange(len(self.values))])
 
 
 # Your RandomizedSet object will be instantiated and called as such:
-# obj = RandomizedSet()
-# param_1 = obj.insert(val)
-# param_2 = obj.remove(val)
-# param_3 = obj.getRandom()
+obj = RandomizedSet()
+obj.insert(1)
+obj.remove(2)
+obj.remove(1)
+obj.insert(2)
+obj.getRandom()
+obj.insert(1)
+obj.insert(2)
+obj.insert(3)
+obj.getRandom()
