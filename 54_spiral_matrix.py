d1004857877a756ac4b2967a6ad4af7a474879b1
lz <ludizh@gmail.com>
2019-08-24 15:26:12 -0700

    update 54 spiral matrix

--- 54_spiral_matrix.py
+++ 54_spiral_matrix.py
@@ -9,6 +9,8 @@ Created on Sat Aug 24 00:04:55 2019
 class Solution:
     def spiralOrder(self, matrix):
         # m: number of column, n: number of row
+        if len(matrix) < 1 or len(matrix[0]) < 1:
+            return([])
         m, n = len(matrix), len(matrix[0])
         min_i = 1
         min_j = 0
@@ -17,22 +19,38 @@ class Solution:
         i = j = 0
         answer = [matrix[i][j]]
         
-        print("min_i, max_i: {}, {}; min_j, max_j: {}, {}".format(min_i, max_i, min_j, max_j))
-        while (min_i <= max_i) or (min_j <= max_j):
-            for j in range(j+1, max_j + 1):
+        if n == 1:
+            for i in range(1, max_i + 1):
                 answer.append(matrix[i][j])
-            max_j -= 1
-            print("min_i, max_i: {}, {}; min_j, max_j: {}, {}".format(min_i, max_i, min_j, max_j))
-            for i in range(i+1, max_i + 1):
-                answer.append(matrix[i][j])
-            max_i -= 1
-            print("min_i, max_i: {}, {}; min_j, max_j: {}, {}".format(min_i, max_i, min_j, max_j))
-            for j in range(j-1, min_j-1, -1):
-                answer.append(matrix[i][j])
-            min_j += 1
-            print("min_i, max_i: {}, {}; min_j, max_j: {}, {}".format(min_i, max_i, min_j, max_j))
-            for i in range(i-1, min_i-1, -1):
-                answer.append(matrix[i][j])
-            min_i += 1
-            print("min_i, max_i: {}, {}; min_j, max_j: {}, {}".format(min_i, max_i, min_j, max_j))
+            return(answer)
+        
+        stop = False
+        while not stop:
+            if j + 1 > max_j:
+                stop = True
+            elif not stop:
+                for j in range(j + 1, max_j + 1):
+                    answer.append(matrix[i][j])
+                max_j -= 1
+            
+            if i + 1 > max_i:
+                stop = True
+            elif not stop:
+                for i in range(i + 1, max_i + 1):
+                    answer.append(matrix[i][j])
+                max_i -= 1
+            
+            if j - 1 < min_j:
+                stop = True
+            elif not stop:
+                for j in range(j - 1, min_j - 1, -1):
+                    answer.append(matrix[i][j])
+                min_j += 1
+            
+            if i - 1 < min_i:
+                stop = True
+            elif not stop:
+                for i in range(i - 1, min_i - 1, -1):
+                    answer.append(matrix[i][j])
+                min_i += 1
         return(answer)
