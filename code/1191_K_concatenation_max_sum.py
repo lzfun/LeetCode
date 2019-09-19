6eccf3623887baf52761a1293a27867e45921ea6
lz <ludizh@gmail.com>
2019-09-18 21:56:12 -0700

    Update 1191_K_concatenation_max_sum.py

--- code/1191_K_concatenation_max_sum.py
+++ code/1191_K_concatenation_max_sum.py
@@ -14,26 +14,30 @@ class Solution(object):
                 bns += x
                 if x > bns: bns = x
                 if bns > ans: ans = bns
-            return ans
+            return(ans)
         
         MOD = 10**9 + 7
-
-
+        
         if K <= 2:
-            return kadane(A) %MOD if K == 1 else kadane(A+A) %MOD
+            return(kadane(A) % MOD if K == 1 else kadane(A+A) % MOD)
         
         S = sum(A)
         if S <= 0:
-            return max(kadane(A), kadane(A+A)) % MOD
+            return(max(kadane(A), kadane(A+A)) % MOD)
+        
         base = S * (K-2) % MOD
         
         prefix = suffix = 0
         su = 0
         for x in A:
             su += x
-            if su > prefix: prefix = su
+            if su > prefix: 
+                prefix = su
+                
         su = 0
+        
         for x in reversed(A):
             su += x
             if su > suffix: suffix = su
-        return (base + prefix + suffix) % MOD
+            
+        return((base + prefix + suffix) % MOD)
